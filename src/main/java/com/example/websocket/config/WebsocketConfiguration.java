20260e4e0ff885e881875a483de365f7cb3ac96b
Satyam0406 <satyamprajapati0406@gmail.com>
2023-09-02 09:04:55 +0530

    Websocket Configuration

---
+++ src/main/java/com/example/websocket/config/WebsocketConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.websocket.config;
+
+import java.net.http.WebSocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addEndpoint("/ws").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		// TODO Auto-generated method stub
+	    registry.enableSimpleBroker("/topic");                        //to receive message
+	    registry.setApplicationDestinationPrefixes("/app");           //to send message
+	}
+	
+
+
+}
