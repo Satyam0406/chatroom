e5ed0f8fcbbd9d2fb82e47016f55772fc0edf1be
Satyam0406 <satyamprajapati0406@gmail.com>
2023-09-02 08:32:06 +0530

    first commit

---
+++ src/main/java/com/example/websocket/WebsocketConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.websocket;
+
+import java.net.http.WebSocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+	
+
+
+}
